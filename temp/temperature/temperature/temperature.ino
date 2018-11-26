9219d922d3e2097bf0d508c0662751487991e78c
Nayana Dasgupta <zcabnda@ucl.ac.uk>
2018-11-26 15:41:43 +0000

    Support for Steinhart-hart

---
+++ temp/temperature/temperature/temperature.ino
@@ -0,0 +1,46 @@
+// desiredTemp comes from user input on control panel
+
+int desiredTemp = 470;
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+  pinMode(9, OUTPUT);
+
+}
+
+void loop() 
+{
+  int currentTemp = analogRead(A0);
+  Serial.println(currentTemp);
+  
+  //if (Serial.available())
+  //{
+    //desiredTemp = serial.read();
+  //}
+  
+  if (desiredTemp < getTemperature(currentTemp))
+  {
+    //turn heater on
+    digitalWrite(9, HIGH);       // sets the digital pin 13 on
+    delay(1000);
+  }
+  else if (desiredTemp > getTemperature(currentTemp))
+  {
+    //turn heater off
+    digitalWrite(9, LOW);       // sets the digital pin 13 on
+    delay(1000);
+  }
+
+  //delay(1000);
+ 
+}
+
+// to convert analog input to degrees celsius using the Steinhart-Hart equation
+double getTemperature(void)
+{
+   double temp = 1/(A + (B * log(10000)) + (C * (log(10000) * log(10000) * log(10000))
+   // Converts Kelvin to Celcius
+   return temp - 273.15;
+}
